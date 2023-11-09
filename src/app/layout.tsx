345d3065450a409c0aaa7fe6783a0069247c4956
Abros Nakhatakyan <a.nakhatakyan@shakes.pro>
2023-11-09 21:58:11 +0300

    fix header nav and input zoom

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,6 +21,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang='ru'>
+      <head>
+        <meta
+          name='viewport'
+          content='width=device-width, initial-scale=1, maximum-scale=1'
+        />
+      </head>
       <body className={classNames(montserrat.className, styles.root)}>
         <Header />
         <main className={styles.main}>{children}</main>
