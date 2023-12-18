3dde75b98bb7ad7ac6144068958611a2520f92e5
Abros Nakhatakyan <a.nakhatakyan@shakes.pro>
2023-12-18 21:30:36 +0300

    fix main page

--- src/components/Main/Advantages/Advantages.tsx
+++ src/components/Main/Advantages/Advantages.tsx
@@ -1,7 +1,12 @@
 import { Title } from '@/components/Title/Title';
 import Image from 'next/image';
 import styles from './Advantages.module.scss';
-import Icon from './img/icon.svg';
+import FreeIcon from './img/free.png';
+import SampleIcon from './img/sample.png';
+import SupportIcon from './img/support.png';
+import TimeIcon from './img/time.png';
+import ToolsIcon from './img/tools.png';
+import UsersIcon from './img/users.png';
 
 export const Advantages = () => {
   return (
@@ -14,7 +19,7 @@ export const Advantages = () => {
             className={styles.advantage}
           >
             <Image
-              src={Icon}
+              src={advantage.image}
               width={80}
               height={80}
               className={styles.icon}
@@ -34,26 +39,32 @@ const ADVANTAGES = [
     title: 'Бесплатно и без регистрации',
     subtitle:
       'Пользуйтесь инструментами без подписок на тарифы абсолютно бесплатно',
+    image: FreeIcon,
   },
   {
     title: 'Простой и удобный интерфейс',
     subtitle: 'Интуитивно понятный интерфейс с минимальной загруженностью',
+    image: SampleIcon,
   },
   {
     title: 'Более 15 инструментов',
     subtitle: 'Каждый месяц разрабатываем для вас новые инструменты',
+    image: ToolsIcon,
   },
   {
     title: 'Экономия времени',
     subtitle: 'Заботимся о том, чтобы вы не тратили время на рутинные задачи',
+    image: TimeIcon,
   },
   {
     title: 'Круглосуточная поддержка',
     subtitle:
       'Наши специалисты работают круглосуточно, мы готовы ответить на ваши вопросы 24/7',
+    image: SupportIcon,
   },
   {
     title: 'Более 1000+ пользователей',
     subtitle: 'Количество пользователей постоянно растет',
+    image: UsersIcon,
   },
 ];
